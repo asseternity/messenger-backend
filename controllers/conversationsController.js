331ab0bd6aaf71860907b61df3acd23b1fa64f92
asseternity <nakupovasset@gmail.com>
2025-03-24 21:36:37 +0500

    feat: create new conversations through admin panel

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -158,7 +158,7 @@ const postNewConversation = async (req, res, next) => {
         await prisma.message.create({
           data: {
             conversationId: fullConversation.id,
-            senderId: req.user.userId,
+            senderId: req.body.userId,
             content: req.body.content,
           },
         });
